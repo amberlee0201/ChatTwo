06d8ec020020e88dee74a4cf7a877c2ed3f45583
bbbng <heoseonho@gmail.com>
2025-02-24 15:41:57 +0900

    :pencil2: fix: Add JsonIgnoreProperties to User entity to prevent serialization errors

--- src/main/java/com/ce/chat2/user/entity/User.java
+++ src/main/java/com/ce/chat2/user/entity/User.java
@@ -1,6 +1,8 @@
 package com.ce.chat2.user.entity;
 
 import com.ce.chat2.common.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,6 +20,8 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" }) // 엔티티를 JSON으로 변환할 때 발생하는 InvalidDefinitionException:
+                                                                 // No serializer found 오류를 피하기 위해 추가
 public class User extends BaseEntity {
 
     @Id
@@ -36,20 +40,20 @@ public class User extends BaseEntity {
 
     UserRole role;
 
-    public static User of(String providerId, String name, String image, UserRole userRole){
+    public static User of(String providerId, String name, String image, UserRole userRole) {
         return User.builder()
-            .providerId(providerId)
-            .name(name)
-            .image(image)
-            .role(userRole)
-            .build();
+                .providerId(providerId)
+                .name(name)
+                .image(image)
+                .role(userRole)
+                .build();
     }
 
     public static User of(OAuth2User loginUser) {
 
         return User.of(loginUser.getAttribute("sub"),
-            loginUser.getAttribute("name"),
-            loginUser.getAttribute("picture"),
-            UserRole.USER);
+                loginUser.getAttribute("name"),
+                loginUser.getAttribute("picture"),
+                UserRole.USER);
     }
 }
