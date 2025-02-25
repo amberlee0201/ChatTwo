5cbf6a3435de2e797a1fb8b19b917c8e8b810c93
loophyun <dhkim960511@naver.com>
2025-02-25 09:44:36 +0900

    :sparkles: feat: implement s3 image crud

--- src/main/java/com/ce/chat2/common/exception/ErrorCode.java
+++ src/main/java/com/ce/chat2/common/exception/ErrorCode.java
@@ -13,8 +13,12 @@ public enum ErrorCode {
     NOT_FOUND(HttpStatus.NOT_FOUND, "GLOBAL_404_ERROR", "404 에러입니다. 요청한 데이터를 서버가 찾을 수 없습니다."),
     REQUEST_IS_NOT_VALID(HttpStatus.BAD_REQUEST, "REQUEST_IS_NOT_VALID","request 값이 잘못되었습니다. 혹은 json 역직렬화를 할 수 없는 형식입니다."),
 
+    //User
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST,"USER_NOT_FOUND", "존재하지 않는 유저입니다."),
-    UNAUTHORIZED_USER(HttpStatus.FORBIDDEN, "UNAUTHORIZED_USER", "권한이 없는 유저입니다.");
+    UNAUTHORIZED_USER(HttpStatus.FORBIDDEN, "UNAUTHORIZED_USER", "권한이 없는 유저입니다."),
+
+    //AWS
+    UNAVAILABLE_S3(HttpStatus.SERVICE_UNAVAILABLE, "UNAVAILABLE_S3", "S3 서비스에러가 발생했습니다."),;
 
     private final HttpStatus status;
     private final String code;
