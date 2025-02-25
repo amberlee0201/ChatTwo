07d6346f1e185843460fb1079a59857fb44e0b2d
loophyun <dhkim960511@naver.com>
2025-02-25 11:26:21 +0900

    :bug: fix: Update OAuth2 user details with updated user info

--- src/main/java/com/ce/chat2/user/controller/UserController.java
+++ src/main/java/com/ce/chat2/user/controller/UserController.java
@@ -6,7 +6,9 @@ import com.ce.chat2.user.exception.UnAuthorizedUser;
 import com.ce.chat2.user.service.UserService;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class UserController {
@@ -33,20 +36,26 @@ public class UserController {
         Model model
     ){
         if(userId != oAuth2User.getUser().getId()) throw new UnAuthorizedUser();
+        log.info("imagePath = {}", oAuth2User.getUser().getImage());
         model.addAttribute("user", oAuth2User.getUser());
         return "myPage";
     }
 
     @PutMapping(value="/users/{userId}", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     String modifyUser(
-        @AuthenticationPrincipal Oauth2UserDetails oauth2User,
+        Authentication authentication,
         @RequestParam("id") int id,
         @RequestParam("name") String name,
         @RequestParam("image") MultipartFile image,
         Model model
     ) throws IOException {
-        User savedUser = userService.updateUserInfo(oauth2User.getUser(),id, name, image);
-        model.addAttribute("user", savedUser);
-        return "myPage";
+        if (authentication.getPrincipal() instanceof Oauth2UserDetails oauthUser) {
+            User savedUser = userService.updateUserInfo(oauthUser, id, name, image);
+            userService.updateOAuth2User(authentication, savedUser);
+            model.addAttribute("user", savedUser);
+            return "myPage";
+        } else {
+            throw new UnAuthorizedUser();
+        }
     }
 }
--- src/main/java/com/ce/chat2/user/service/UserService.java
+++ src/main/java/com/ce/chat2/user/service/UserService.java
@@ -1,5 +1,6 @@
 package com.ce.chat2.user.service;
 
+import com.ce.chat2.common.oauth.Oauth2UserDetails;
 import com.ce.chat2.common.s3.S3FileDto;
 import com.ce.chat2.common.s3.S3Service;
 import com.ce.chat2.user.entity.User;
@@ -7,8 +8,13 @@ import com.ce.chat2.user.exception.UnAuthorizedUser;
 import com.ce.chat2.user.exception.UserNotFound;
 import com.ce.chat2.user.repository.UserRepository;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -23,9 +29,9 @@ public class UserService {
     private final S3Service s3Service;
 
     @Transactional
-    public User updateUserInfo(User loginUser,int id, String name, MultipartFile image)
+    public User updateUserInfo(Oauth2UserDetails loginUser,int id, String name, MultipartFile image)
         throws IOException {
-        if(loginUser.getId() != id) throw new UnAuthorizedUser();
+        if(loginUser.getUser().getId() != id) throw new UnAuthorizedUser();
         User savedUser = userRepository.findById(id)
             .orElseThrow(UserNotFound::new);
 
@@ -38,6 +44,24 @@ public class UserService {
 
         savedUser.updateInfo(name, dto);
         return savedUser;
+    }
+
+    public void updateOAuth2User(Authentication authentication, User updatedUser) {
+        if (authentication instanceof OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+            String provider = oAuth2AuthenticationToken.getAuthorizedClientRegistrationId();
+
+            if (authentication.getPrincipal() instanceof Oauth2UserDetails customOAuth2User) {
+                Map<String, Object> attributes = new HashMap<>(customOAuth2User.getAttributes());
+
+                attributes.put("name", updatedUser.getName());
+                attributes.put("picture", updatedUser.getImage());
+                Oauth2UserDetails updatedOAuth2User = new Oauth2UserDetails(updatedUser, attributes);
+
+                SecurityContextHolder.getContext().setAuthentication(
+                    new OAuth2AuthenticationToken(updatedOAuth2User, customOAuth2User.getAuthorities(), provider)
+                );
+            }
+        }
     }
 }
 
