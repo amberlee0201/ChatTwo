53e2c21e79f89205d88396210a5de7f5267b98fc
bbbng <heoseonho@gmail.com>
2025-02-25 09:55:31 +0900

    :sparkles: feat: Add functionality to add new friends in ApiFriendsController

--- src/main/java/com/ce/chat2/friend/controller/ApiFriendsController.java
+++ src/main/java/com/ce/chat2/friend/controller/ApiFriendsController.java
@@ -13,7 +13,11 @@ import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +26,7 @@ public class ApiFriendsController {
         private final FollowRepository followRepository;
 
         @GetMapping("/api/friends")
-        public List<Follow> friendsList() {
+        public ResponseEntity<List<Follow>> friendsList() {
 
                 // 현재 로그인한 유저를 찾는 기능 필요.
                 User currentUser = userRepository.findById(1).get();
@@ -30,7 +34,20 @@ public class ApiFriendsController {
                 List<Follow> friends = new ArrayList<Follow>();
                 followRepository.getFollowsByFrom(currentUser).forEach(friends::add);
 
-                return friends;
+                return ResponseEntity.ok(friends);
+        }
+
+        @PostMapping("/api/friends")
+        public ResponseEntity<Follow> addFriend(@RequestPart("friendName") String friendName) {
+                // 현재 로그인한 유저를 찾는 기능 필요.
+                User currentUser = userRepository.findById(1).get();
+
+                User friend = userRepository.findByName(friendName);
+
+                Follow follow = Follow.builder().from(currentUser).to(friend).isBreak(false).build();
+                followRepository.save(follow);
+
+                return ResponseEntity.ok(follow);
         }
 
 }
--- src/main/java/com/ce/chat2/user/repository/UserRepository.java
+++ src/main/java/com/ce/chat2/user/repository/UserRepository.java
@@ -9,4 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByProviderId(String id);
+
+    User findByName(String friendName);
 }
