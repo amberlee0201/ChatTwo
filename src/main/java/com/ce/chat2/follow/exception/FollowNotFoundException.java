e7e8b3838d2545597487fdff610a0de6624df5e2
bbbng <heoseonho@gmail.com>
2025-02-26 22:27:50 +0900

    ✏ fix: Update FollowNotFoundException to use default error message

--- src/main/java/com/ce/chat2/follow/exception/FollowNotFoundException.java
+++ src/main/java/com/ce/chat2/follow/exception/FollowNotFoundException.java
@@ -4,8 +4,8 @@ import com.ce.chat2.common.exception.CustomBaseException;
 import com.ce.chat2.common.exception.ErrorCode;
 
 public class FollowNotFoundException extends CustomBaseException {
-    public FollowNotFoundException(String message) {
-        super(message, ErrorCode.FOLLOW_NOT_FOUND);
+    public FollowNotFoundException() {
+        super(ErrorCode.FOLLOW_NOT_FOUND);
     }
 
 }
