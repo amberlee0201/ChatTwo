6e9721e01b6e63b53d4f4c29fb937e7887ab2622
bbbng <heoseonho@gmail.com>
2025-02-26 22:31:09 +0900

    ✏ refactor: Update ApiFollowController to return UserResponse DTOs and enhance follow management methods

--- src/main/java/com/ce/chat2/follow/controller/ApiFollowController.java
+++ src/main/java/com/ce/chat2/follow/controller/ApiFollowController.java
@@ -3,11 +3,9 @@ package com.ce.chat2.follow.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ce.chat2.common.oauth.Oauth2UserDetails;
-import com.ce.chat2.follow.entity.Follow;
 import com.ce.chat2.follow.service.FollowService;
-import com.ce.chat2.user.entity.User;
-import com.ce.chat2.user.exception.UserNotFound;
-import com.ce.chat2.user.repository.UserRepository;
+import com.ce.chat2.user.dto.UserResponse;
+import com.ce.chat2.user.exception.UnAuthorizedUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,50 +23,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class ApiFollowController {
         private final FollowService followService;
-        private final UserRepository userRepository;
 
         // 친구 목록을 가져오는 API - dto 반환하도록 변경할 것.
         @GetMapping("/api/friends/{uid}")
-        public ResponseEntity<List<Follow>> getFollow(@AuthenticationPrincipal Oauth2UserDetails userDetails) {
-                // uid와 userDetails의 uid가 일치하는지 확인하는 코드
-                // if (uid != userDetails.getUid()) {
-                // throw new IllegalArgumentException("uid와 userDetails의 uid가 일치하지 않습니다.");
-
-                List<Follow> friends = followService.getFollow(userDetails.getUser());
-                return ResponseEntity.ok(friends);
+        public ResponseEntity<List<UserResponse>> getFollow(@PathVariable("uid") Integer uid,
+                        @AuthenticationPrincipal Oauth2UserDetails userDetails) {
+                // uid와 userDetails의 uid가 일치하는지 확인
+                if (uid != userDetails.getUser().getId()) {
+                        throw new UnAuthorizedUser();
+                }
+                return ResponseEntity.ok(followService.getFollow(userDetails.getUser()));
         }
 
         // 친구 추가 API - dto 반환하도록 변경할 것.
         @PostMapping("/api/friends/{uid}")
-        public ResponseEntity<User> setFollow(@PathVariable("uid") Integer uid,
+        public ResponseEntity<UserResponse> setFollow(@PathVariable("uid") Integer uid,
                         @AuthenticationPrincipal Oauth2UserDetails userDetails) {
-                User friend = userRepository.findById(uid).orElseThrow(() -> new UserNotFound());
 
-                followService.setFollow(userDetails.getUser(), friend);
-
-                return ResponseEntity.ok(friend);
+                return ResponseEntity.ok(followService.setFollow(userDetails.getUser(), uid));
         }
 
         // 친구 삭제 API - dto 반환하도록 변경할 것.
         @DeleteMapping("/api/friends/{uid}")
-        public ResponseEntity<User> deleteFollow(@PathVariable("uid") Integer uid,
+        public ResponseEntity<UserResponse> deleteFollow(@PathVariable("uid") Integer uid,
                         @AuthenticationPrincipal Oauth2UserDetails userDetails) {
 
-                User friend = userRepository.findById(uid).orElseThrow(() -> new UserNotFound());
-
-                followService.deleteFollow(userDetails.getUser(), friend);
-
-                return ResponseEntity.ok(friend);
+                return ResponseEntity.ok(followService.deleteFollow(userDetails.getUser(), uid));
         }
 
         // 친구 찾기 API - dto 반환하도록 변경할 것.
         @GetMapping("/api/friends/find")
-        public ResponseEntity<List<User>> findFollow(@RequestParam("name") String name,
+        public ResponseEntity<List<UserResponse>> findFollow(@RequestParam("name") String name,
                         @AuthenticationPrincipal Oauth2UserDetails userDetails) {
 
-                List<User> users = followService.findFollow(userDetails.getUser(), name);
-
-                return ResponseEntity.ok(users);
+                return ResponseEntity.ok(followService.findFollow(userDetails.getUser(), name));
         }
 
 }
