62f79303bda4e9ad5dfe23cd032c84d539aa8343
bbbng <heoseonho@gmail.com>
2025-02-26 17:34:05 +0900

    :pencil2: fix: Update FollowRepository to use custom queries for retrieving follows and improve data handling

--- src/main/java/com/ce/chat2/follow/repository/FollowRepository.java
+++ src/main/java/com/ce/chat2/follow/repository/FollowRepository.java
@@ -1,8 +1,11 @@
 package com.ce.chat2.follow.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ce.chat2.follow.entity.Follow;
@@ -11,8 +14,12 @@ import com.ce.chat2.user.entity.User;
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
 
-    Iterable<Follow> getFollowsByFrom(User from);
+    @Query("SELECT f FROM Follow f WHERE f.from = :from AND f.isBreak = false")
+    List<Follow> getFollowsByFrom(User from);
 
     Optional<Follow> findByFromAndTo(User currentUser, User friend);
+
+    @Query("SELECT f.to FROM Follow f WHERE f.from = :currentUser AND f.to.name = :name AND f.isBreak = false")
+    List<User> findFollowsByUserAndName(@Param("currentUser") User currentUser, @Param("name") String name);
 
 }
