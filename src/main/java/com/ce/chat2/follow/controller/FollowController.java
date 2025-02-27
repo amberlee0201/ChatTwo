d6a9bd41f6e469a6acc77d59f62a43a746b1e5eb
bbbng <heoseonho@gmail.com>
2025-02-27 11:22:36 +0900

    ✏ fix: friends list functionality with user details and update friend data handling

--- src/main/java/com/ce/chat2/follow/controller/FollowController.java
+++ src/main/java/com/ce/chat2/follow/controller/FollowController.java
@@ -1,13 +1,18 @@
 package com.ce.chat2.follow.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ce.chat2.common.oauth.Oauth2UserDetails;
 
 @Controller
 public class FollowController {
 
     @GetMapping("/friends")
-    String friendsList() {
+    String friendsList(@AuthenticationPrincipal Oauth2UserDetails userDetails, Model model) {
+        model.addAttribute("user", userDetails.getUser());
         return "followList";
     }
 
