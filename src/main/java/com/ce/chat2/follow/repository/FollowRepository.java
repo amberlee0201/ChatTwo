db6325105c167ae00ff39b8571a24f3e6e597711
bbbng <heoseonho@gmail.com>
2025-02-27 15:31:06 +0900

    ✏ fix: Optimize FollowRepository queries by using JOIN FETCH for better performance

--- src/main/java/com/ce/chat2/follow/repository/FollowRepository.java
+++ src/main/java/com/ce/chat2/follow/repository/FollowRepository.java
@@ -15,14 +15,14 @@ import com.ce.chat2.user.entity.User;
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
 
     // 현재 친구 목록
-    @Query("SELECT f FROM Follow f WHERE f.from = :from AND f.isBreak = false")
+    @Query("SELECT f FROM Follow f JOIN FETCH f.to WHERE f.from = :from AND f.isBreak = false")
     List<Follow> findByFrom(User from);
 
     // 친구 추가/삭제를 위한 검색
     Optional<Follow> findByFromAndTo(User from, User to);
 
     // 친구 목록에서 친구를 찾아 친구 이름으로 검색, 조인 쿼리 사용
-    @Query("SELECT f FROM Follow f JOIN f.to u WHERE f.from = :from AND u.name LIKE :name AND f.isBreak = false")
+    @Query("SELECT f FROM Follow f JOIN FETCH f.to u WHERE f.from = :from AND u.name LIKE :name AND f.isBreak = false")
     List<Follow> findByFromAndName(@Param("from") User from, @Param("name") String name);
 
 }
