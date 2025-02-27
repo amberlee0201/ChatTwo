8de05c310772f7ef8ece442b74a3a7d00b1340ba
amberlee0201 <amber.lee.020100@gmail.com>
2025-02-27 11:49:53 +0900

    :pencil2: fix: bug fix

--- src/main/java/com/ce/chat2/participation/controller/ParticipationController.java
+++ src/main/java/com/ce/chat2/participation/controller/ParticipationController.java
@@ -2,16 +2,12 @@ package com.ce.chat2.participation.controller;
 
 import com.ce.chat2.common.oauth.Oauth2UserDetails;
 import com.ce.chat2.participation.service.ParticipationService;
-import com.ce.chat2.user.exception.USER_NOT_FOUND;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -19,16 +15,17 @@ import java.util.List;
 public class ParticipationController {
 
     private final ParticipationService participationService;
+    private final SimpMessageSendingOperations messagingTemplate;
 
     // 존 채팅방 목록 요청 (처음 화면 로딩 시 최초 1회)
     @MessageMapping("/rooms/init")
-    @SendTo("/topic/user/{userId}")
-    public List<String> sendMessage(Authentication authentication) {
+    public void sendMessage(Authentication authentication) {
 
         if (authentication.getPrincipal() instanceof Oauth2UserDetails userDetails) {
             userDetails = (Oauth2UserDetails) authentication.getPrincipal();
             String userId = String.valueOf(userDetails.getUser().getId());
-            return participationService.sendInitialRooms(userId);
+            messagingTemplate.convertAndSend("/topic/user/" + userId,
+                    participationService.sendInitialRooms(userId));
         } else {
             // @TODO
             throw new ClassCastException("authentication.getPrincipal() is not instanceof " + Oauth2UserDetails.class.getSimpleName());
