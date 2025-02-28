878e6a8223ce7a471e82d666540e0d82e0aca801
bbbng <heoseonho@gmail.com>
2025-02-28 09:39:41 +0900

    ✏ refactor: Move friend-related queries from FollowRepository to UserRepository for better organization

--- src/main/java/com/ce/chat2/follow/repository/FollowRepository.java
+++ src/main/java/com/ce/chat2/follow/repository/FollowRepository.java
@@ -1,28 +1,11 @@
 package com.ce.chat2.follow.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ce.chat2.follow.entity.Follow;
-import com.ce.chat2.user.entity.User;
 
 @Repository
 public interface FollowRepository extends JpaRepository<Follow, Integer> {
-
-    // 현재 친구 목록
-    @Query("SELECT f FROM Follow f JOIN FETCH f.to WHERE f.from = :from AND f.isBreak = false")
-    List<Follow> findByFrom(User from);
-
-    // 친구 추가/삭제를 위한 검색
-    Optional<Follow> findByFromAndTo(User from, User to);
-
-    // 친구 목록에서 친구를 찾아 친구 이름으로 검색, 조인 쿼리 사용
-    @Query("SELECT f FROM Follow f JOIN FETCH f.to u WHERE f.from = :from AND u.name LIKE :name AND f.isBreak = false")
-    List<Follow> findByFromAndName(@Param("from") User from, @Param("name") String name);
 
 }
--- src/main/java/com/ce/chat2/user/repository/UserRepository.java
+++ src/main/java/com/ce/chat2/user/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.ce.chat2.user.repository;
 
+import com.ce.chat2.follow.entity.Follow;
 import com.ce.chat2.user.entity.User;
 
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,4 +18,15 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     User findByName(String name);
 
     List<User> findByNameContaining(String namePart);
+
+    // 현재 친구 목록
+    @Query("SELECT u FROM User u JOIN Follow f on f.to = u WHERE f.from = :fromUser AND f.isBreak = false")
+    List<Follow> findByFrom(User fromUser);
+
+    // 친구 추가/삭제를 위한 검색
+    Optional<Follow> findByFromAndTo(User from, User to);
+
+    // 친구 목록에서 친구를 찾아 친구 이름으로 검색, 조인 쿼리 사용 -> 유저 먼저 읽도록 수정하자.
+    @Query("SELECT u FROM User u JOIN Follow f on f.to = u WHERE f.from = :fromUser AND u.name LIKE :name AND f.isBreak = false")
+    List<Follow> findByFromAndName(@Param("from") User fromUser, @Param("name") String name);
 }
