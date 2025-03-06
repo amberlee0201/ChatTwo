62ae43411f6d6d902b0eb81c3cfbc97176852581
bbbng <heoseonho@gmail.com>
2025-03-06 11:45:44 +0900

    ✨ feat: update configuration to use chat2.site URL instead of hardcoded localhost

--- src/main/java/com/ce/chat2/common/config/StompWebSocketConfig.java
+++ src/main/java/com/ce/chat2/common/config/StompWebSocketConfig.java
@@ -1,5 +1,6 @@
 package com.ce.chat2.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -10,10 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Value("${cloud.aws.url}")
+    private String url;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/connect")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins(url)
                 .withSockJS();
     }
 
