cfbddde89168338d2f66fe15622f10b2df2dc4fe
bbbng <heoseonho@gmail.com>
2025-03-11 15:43:12 +0900

    ✨ feat: allow public access to actuator endpoints

--- src/main/java/com/ce/chat2/common/config/SecurityConfig.java
+++ src/main/java/com/ce/chat2/common/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/admin/**").hasRole("ADMIN") //admin 가능 경로
                 .requestMatchers("/","/static/**", "/images/**").permitAll() //login 전 가능 페이지
+                .requestMatchers("/chat2-actuator/**").permitAll() // actuator 페이지
                 .anyRequest().authenticated() // 그 이외는 인증 필요
             )
             .formLogin(AbstractHttpConfigurer::disable)
