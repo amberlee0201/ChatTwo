7c60053f46757f34ca5445ca286c37bacb73b4a6
amberlee0201 <amber.lee.020100@gmail.com>
2025-03-12 15:21:05 +0900

    :sparkles: feat: real time chat room preview update

--- src/main/java/com/ce/chat2/chat/service/RedisChatPubSubService.java
+++ src/main/java/com/ce/chat2/chat/service/RedisChatPubSubService.java
@@ -7,8 +7,11 @@ import com.ce.chat2.chat.exception.ChatSendException;
 import com.ce.chat2.chat.repository.ChatRepository;
 import com.ce.chat2.participation.entity.Participation;
 import com.ce.chat2.participation.repository.ParticipationRepository;
+import com.ce.chat2.room.dto.response.RoomResponse;
+import com.ce.chat2.room.entity.Room;
 import com.ce.chat2.room.exception.RoomNotFoundException;
 import com.ce.chat2.room.repository.RoomRepository;
+import com.ce.chat2.room.service.RoomWebSocketService;
 import com.ce.chat2.user.entity.User;
 import com.ce.chat2.user.exception.UserNotFound;
 import com.ce.chat2.user.repository.UserRepository;
@@ -16,12 +19,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RedisChatPubSubService implements MessageListener {
@@ -31,6 +36,7 @@ public class RedisChatPubSubService implements MessageListener {
     private final ParticipationRepository participationRepository;
     private final StringRedisTemplate stringRedisTemplate;
     private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final RoomWebSocketService roomWebSocketService;
 
     public void publish(String channel, String msg){
         stringRedisTemplate.convertAndSend(channel, msg);
@@ -43,8 +49,10 @@ public class RedisChatPubSubService implements MessageListener {
         try{
             ChatRequestDto dto = om.readValue(payload, ChatRequestDto.class);
             User sender = userRepository.findById(dto.getUserId()).orElseThrow(UserNotFound::new);
-            roomRepository.findRoomById(dto.getRoomId()).orElseThrow(RoomNotFoundException::new);
+            Room room = roomRepository.findRoomById(dto.getRoomId()).orElseThrow(RoomNotFoundException::new);
             Chat chat = chatRepository.save(Chat.of(dto));
+
+            updateRoom(room, chat);
 
             List<Participation> participationList = participationRepository.findAllByRoomId(dto.getRoomId());
 
@@ -53,5 +61,10 @@ public class RedisChatPubSubService implements MessageListener {
         }catch (JsonProcessingException e){
             throw new ChatSendException(e.getMessage());
         }
+    }
+
+    private void updateRoom(Room room, Chat chat){
+        Room updatedRoom = roomRepository.update(Room.from(room, chat));
+        roomWebSocketService.notifyUsersAboutUpdatedRoom(RoomResponse.of(updatedRoom));
     }
 }
